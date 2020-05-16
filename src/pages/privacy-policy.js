f44d2535733681ae8d388f5cb56815d431058a37
Davide Crestini <davide.crestini@theculturetrip.com>
2020-05-16 21:24:57 +0100

    added images and privacy policy page

---
+++ src/pages/privacy-policy.js
@@ -0,0 +1,12 @@
+import React from "react"
+import SEO from "../components/seo"
+
+const PrivacyPolicyPage = () => (
+  <>
+    <SEO title="Privacy Policy" />
+    <h1>Privacy Policy</h1>
+    <p>Lorem ipsum.</p>
+  </>
+)
+
+export default PrivacyPolicyPage
